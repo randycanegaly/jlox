a86bc1d6d386c2a5dc137716732cb6d5c87f9d7a
randycanegaly <canegaly@gmail.com>
2025-02-26 18:08:53 -0500

    Added error handling to Lox class and its methods.

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -7,9 +7,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-//Testing here Eclipse git integration ????
-
 public class Lox {
+	static boolean hadError = false;
+	
 	public static void main(String[] args) throws IOException {
 		if (args.length > 1) {
 			System.out.println("Usage: jlox [script]");
@@ -24,6 +24,8 @@ public class Lox {
 	private static void runFile(String path) throws IOException {
 		byte[] bytes = Files.readAllBytes(Paths.get(path));
 		run(new String(bytes, Charset.defaultCharset()));
+		
+		if (hadError) System.exit(65);
 	}
 
 	private static void runPrompt() throws IOException {
@@ -35,6 +37,8 @@ public class Lox {
 			String line = reader.readLine();
 			if (line == null) break;
 			run(line);
+			
+			hadError = false;
 		}
 	}
 	 
@@ -45,5 +49,10 @@ public class Lox {
 		for (Token token : tokens) {
 			System.out.println(token);
 		}
+	}
+	
+	static void error(int line, String where, String message) {
+		System.err.println("[line " + line + "] Error" + where + ": " + message);
+		hadError = true;
 	}
 }
