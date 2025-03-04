1ba4ce75bffde862a08e756318f31bc1b9a7a1a6
randycanegaly <canegaly@gmail.com>
2025-03-03 22:21:58 -0500

    Oops. Forgot one.

---
+++ src/com/craftinginterpreters/lox/Stmt.java
@@ -0,0 +1,42 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+
+abstract class Stmt {
+
+	//Declare an abstract method that takes Visitor type. All subclasses of baseName implement this method. 
+	abstract <R> R accept(Visitor<R> visitor);
+
+	/* 	The Visitor interface defines a set of methods, one for each type
+   		such that some action can be enabled across all types, minimizing in-type edits to implement the methods. */
+	interface Visitor<R> {//visit method prototypes for each type. Using generics.
+		R visitExpressionStmt(Expression stmt);
+		R visitPrintStmt(Print stmt);
+ 	}
+
+	//Expression
+	static class Expression extends Stmt {
+		Expression(Expr expression) {
+			this.expression = expression;
+		}
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitExpressionStmt(this);
+		}
+
+		final Expr expression;
+	}
+
+	//Print
+	static class Print extends Stmt {
+		Print(Expr expression) {
+			this.expression = expression;
+		}
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitPrintStmt(this);
+		}
+
+		final Expr expression;
+	}
+}
